857468bca334a90cf85c053af50550b2b15eff41
Suphanath Wangthip <suphanath_w@cmu.ac.th>
2022-09-24 14:06:29 +0700

    finish

--- pages/api/room.js
+++ pages/api/room.js
@@ -2,4 +2,18 @@ import { readDB } from "../../backendLibs/dbLib";
 
 export default function roomRoute(req, res) {
   const rooms = readDB();
+
+  const result = [];
+  for (const room of rooms) {
+    result.push({
+      roomId: room.roomId,
+      roomName: room.roomName,
+    });
+  }
+  if (req.method === "GET") {
+    return res.json({
+      ok: true,
+      result,
+    });
+  }
 }
